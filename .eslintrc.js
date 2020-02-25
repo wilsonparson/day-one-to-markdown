f4a0f324a565b191ab1995f8a3a1d83fb5fb428c
Wilson Parson <wilson.parson@gmail.com>
2020-02-24 17:25:07 -0700

    Configure prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module' // Allows for the use of imports
-  }
-}
+    sourceType: 'module', // Allows for the use of imports
+  },
+};
